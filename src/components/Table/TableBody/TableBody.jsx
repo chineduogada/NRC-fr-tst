84f15c05c6d63c8045a505fd9fcf9d3e791b8044
chineduogada <chineduogada@gmail.com>
2020-01-07 15:53:24 +0100

    refractor of employees component with links

--- src/components/Table/TableBody/TableBody.jsx
+++ src/components/Table/TableBody/TableBody.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const TableBody = ({data, columns}) => {
+const TableBody = ({ data, columns }) => {
 	return (
 		<div className="TableBody">
 			{data.map(data => (
-				<div key={data.id} className="TableRow">
+				<Link to={`/employees/${data.id}`} key={data.id} className="TableRow">
 					{columns.map(column => (
 						<div className="TableCol" key={column.title + data.id}>
 							<h6>{data[column.title]}</h6>
 						</div>
 					))}
-				</div>
+				</Link>
 			))}
 		</div>
 	);
