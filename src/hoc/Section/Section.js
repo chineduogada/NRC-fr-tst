07c2a1cfaa96ee707057e7583bde7c3fd4bb5417
chineduogada <chineduogada@gmail.com>
2020-01-08 12:49:30 +0100

    refractor(layout):update component used hoc Section component once in Layout component

--- src/hoc/Section/Section.js
+++ src/hoc/Section/Section.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import classes from './Section.module.css';
+import React from "react";
+import classes from "./Section.module.css";
 
-const section = (props) => {
-  const titleExists = props.title !== undefined;
-  return (
-    <section className={classes.Section} id={props.id}>
-      {titleExists ? <h2 className={classes.SectionTitle}>{props.title}</h2> : null}
+const section = props => {
+	const titleExists = props.title !== undefined;
+	return (
+		<section className={classes.Section} id={props.id}>
+			{titleExists ? (
+				<h2 className={classes.SectionTitle}>{props.title}</h2>
+			) : null}
 
-      {props.children}
-    </section>
-  );
-}
+			{props.children}
+		</section>
+	);
+};
 
 export default section;
