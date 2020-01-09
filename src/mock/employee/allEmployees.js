af4b553fa41e82bce95efbc8503a02972331d0d6
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-01-09 14:11:00 +0100

    chore(mock):add employee mock data

---
+++ src/mock/employee/allEmployees.js
@@ -0,0 +1,53 @@
+export default {
+  status: 'success',
+  message: 'query successful',
+  data: {
+    count: 2,
+    rows: [
+      {
+        id: 3,
+        ippisNo: 153941,
+        firstName: 'uiouirhoi',
+        lastName: 'vsdvsv',
+        employeeJob: {
+          section: 2,
+          employeeStatus: 'A',
+          pensionable: 'Y',
+          department: {
+            description: 'Administration and Human Resources'
+          },
+          district: {
+            siteName: 'Head Office'
+          },
+          reportToEmployee: {
+            ippisNo: 153941,
+            firstName: 'uiouirhoi',
+            lastName: 'vsdvsv'
+          }
+        }
+      },
+      {
+        id: 5,
+        ippisNo: 94321,
+        firstName: 'uiouirhoi',
+        lastName: 'vsdvsv',
+        employeeJob: {
+          section: 2,
+          employeeStatus: 'A',
+          pensionable: 'Y',
+          department: {
+            description: 'Administration and Human Resources'
+          },
+          district: {
+            siteName: 'Head Office'
+          },
+          reportToEmployee: {
+            ippisNo: 153941,
+            firstName: 'uiouirhoi',
+            lastName: 'vsdvsv'
+          }
+        }
+      }
+    ]
+  }
+};
