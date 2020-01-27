271a935cb20d24e274a0f6b9c2d2ed0448d7e0c8
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-01-27 09:50:20 +0100

    feature(users):implement dropdown

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Dropdown.module.scss';
 // import DropdownItems from './DropdownItems/DropdownItems';
-import { IoIosArrowDropdown } from 'react-icons/io';
+import { IoIosArrowDown } from 'react-icons/io';
 
-const dropdown = props => {
-  return (
-    <div className={classes.Dropdown}>
-      <div className={classes.MenuIcon}>
-        {props.trigger} <IoIosArrowDropdown />
+class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      drop: false,
+      parent: null
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleParentClick = this.handleParentClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.DropDown.parentElement.onclick = this.handleParentClick;
+    this.setState({ parent: this.DropDown.parentElement });
+  }
+
+  handleParentClick(event) {
+    event.stopPropagation();
+    this.setState({ drop: false });
+  }
+
+  handleClick(event) {
+    event.stopPropagation();
+    this.setState({ drop: !this.state.drop });
+  }
+
+  render() {
+    const { drop } = this.state;
+    console.log(drop);
+    return (
+      <div
+        className={classes.Dropdown}
+        ref={dropdown => (this.DropDown = dropdown)}
+      >
+        <button className={classes.Trigger} onClick={this.handleClick}>
+          {this.props.trigger}
+          <IoIosArrowDown className={classes.TriggerIcon} />
+        </button>
+
+        {/* Dropdown Items */}
+        <div className={`${classes.Items} ${drop ? classes.Drop : null}`}>
+          {this.props.children}
+        </div>
       </div>
-
-      {/* Dropdown Items */}
-      <div className={classes.Items}>{props.children}</div>
-    </div>
-  );
-};
-
-export default dropdown;
+    );
+  }
+}
+export default Dropdown;
