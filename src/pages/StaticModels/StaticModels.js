62085140854bf3972865425b4a093532a852e129
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-01-31 19:00:05 +0100

    feature(static-models):start static models comp

--- src/pages/StaticModels/StaticModels.js
+++ src/pages/StaticModels/StaticModels.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { IoIosArrowBack }  from 'react-icons/io';
+import { IoIosArrowBack, IoIosLink } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 import SimpleTabs from '../../components/SimpleTabs/SimpleTabs';
 import Section from '../../hoc/Section/Section';
 import classes from './StaticModels.module.scss';
 
 class StaticModels extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isActive: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
 
-    render() {
-        return (
-            <Section title={`${this.state.isActive} static models`}></Section>
-        )
-    }
+    this.models = [{ name: 'districts', url: 'districts' }];
+  }
+
+  renderModels() {
+    return (
+      <ul className={classes.Models}>
+        {this.models.map((model, i) => {
+          return (
+            <li key={i} className={classes.Model}>
+              <Link to={`/settings/static-models/${model.url}`}>
+                {model.name}
+                <IoIosLink className={classes.LinkIcon} />
+              </Link>
+              <p className={classes.ModelDescription}>
+                Add and modify {model.name}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <Section title={`${this.state.isActive} static models`}>
+        <br />
+        {this.renderModels()}
+      </Section>
+    );
+  }
 }
 
 export default withRouter(StaticModels);
-
-
